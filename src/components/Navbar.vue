3cf4ad65cc20487ef6fc552d27d1c636413b4f44
Esther Bauzou <48768205+PetitChampi@users.noreply.github.com>
2022-04-16 11:30:05 +0200

    navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there DISPLAY NAME</p>
+      <p class="email">Currently logged in as EMAIL</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
